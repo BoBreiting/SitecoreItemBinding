0e5a2aac73a5b84bcdb5d934926389ca4f71a4a6
BoBreiting <jivefive@gmail.com>
2014-11-10 20:44:03 +0100

    Updated documentation

--- ItemBinding/Model/BindingContracts/CompositeTypeAttribute.cs
+++ ItemBinding/Model/BindingContracts/CompositeTypeAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace ItemBinding.Model.BindingContracts
 {
+  /// <summary>
+  /// Represents a clause attribute for specifying composite type members.
+  /// </summary>
   [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
   public class CompositeTypeAttribute : Attribute
   {
@@ -10,6 +13,12 @@ namespace ItemBinding.Model.BindingContracts
       Type = type;
     }
 
+    /// <summary>
+    /// Gets the type of the composite member.
+    /// </summary>
+    /// <value>
+    /// The type of the composite member.
+    /// </value>
     public Type Type { get; private set; }
   }
 }
